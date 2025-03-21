f12e855bc9c719d5f0bb00308f67ffce87b8372d
Pranjal Singh <prancoder@gmail.com>
2025-03-21 17:34:37 +0530

    Setup route for stats fetching

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV != "production") {
+if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
 
@@ -10,15 +10,19 @@ const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
-const passport = require('passport'); 
-const LocalStrategy = require('passport-local');
-const User = require('./models/users');
-const MongoStore = require('connect-mongo');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+
+const Listing = require("./models/listings");
+const Review = require("./models/reviews");
+const User = require("./models/users");
+
+const MongoStore = require("connect-mongo");
 
 // Import routes
-const listingRoutes = require('./routes/listing');
-const reviewRoutes = require('./routes/review');
-const userRoutes = require('./routes/user');
+const listingRoutes = require("./routes/listing");
+const reviewRoutes = require("./routes/review");
+const userRoutes = require("./routes/user");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -42,27 +46,27 @@ async function main() {
 }
 
 const store = MongoStore.create({
-  mongoUrl : MONGO_URL,
-  crypto : {
-    secret : process.env.SECRET
+  mongoUrl: MONGO_URL,
+  crypto: {
+    secret: process.env.SECRET,
   },
-  touchAfter : 24*3600,
+  touchAfter: 24 * 3600,
 });
 
-store.on("error", function(error) {
-  console.log("Error in Mongo Session Store", error)
-})
+store.on("error", function (error) {
+  console.log("Error in Mongo Session Store", error);
+});
 
 // Session configuration
 const sessionOptions = {
   store,
-  secret : process.env.SECRET,
-  resave : false,
-  saveUninitialized : true,
-  cookie : {
-    expires : Date.now() + 7*24*60*60*1000,
-    maxAge : 7*24*60*60*1000,
-    httpOnly : true,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
   },
 };
 
@@ -76,7 +80,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // Middleware to set local variables
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   res.locals.warning = req.flash("warning");
@@ -95,28 +99,45 @@ app.use("/listings/:id/reviews", reviewRoutes);
 app.use("/", userRoutes);
 
 // Static pages
-app.get("/terms", (req,res)=>{
+app.get("/terms", (req, res) => {
   res.render("pages/terms.ejs");
 });
 
-app.get("/privacy", (req,res) =>{
+app.get("/privacy", (req, res) => {
   res.render("pages/privacy.ejs");
 });
 
-app.get("/about", (req,res) =>{
-  res.render("pages/about.ejs");
+app.get("/about", async (req, res) => {
+  try {
+    const propertyCount = await Listing.countDocuments();
+    const userCount = await User.countDocuments();
+    const reviewCount = await Review.countDocuments();
+
+    res.render("pages/about.ejs", {
+      propertyCount,
+      userCount,
+      reviewCount,
+    });
+  } catch (error) {
+    console.error("Error fetching counts:", error);
+    res.render("pages/about.ejs", {
+      propertyCount: 0,
+      userCount: 0,
+      reviewCount: 0,
+    });
+  }
 });
 
 // Custom error handling middleware
-app.use((err,req,res,next) =>{
-  let {statusCode = 500, message = "Some error occured"} = err;
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Some error occured" } = err;
   console.error(err.message);
   res.status(statusCode).render("./layouts/error.ejs", { message });
 });
 
 app.use((req, res, next) => {
-  let message = "Oops! The page you requested could not be found"
-  res.status(404).render("./layouts/error.ejs", {message});
+  let message = "Oops! The page you requested could not be found";
+  res.status(404).render("./layouts/error.ejs", { message });
 });
 
 app.listen((port = 3000), () => {
